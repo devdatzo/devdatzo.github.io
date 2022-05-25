87a5f85c28dc2860744d87eb00b7ea2fa93e703d
devdatzo <devdatzo@gmail.com>
2022-05-26 05:42:30 +0700

    portfolio HTML+CSS+JS

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+const humberger = document.querySelector(".humberger");
+const navMenu = document.querySelector(".nav-menu");
+
+humberger.addEventListener("click", ()=> {
+  humberger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}) 
+document.querySelectorAll(".nav-link").forEach(n=>n.addEventListener("click",()=>{
+  humberger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+
+const activePage = window.location.pathname;
+const navLink = document.querySelectorAll(".nav-link").forEach(link =>{
+   if(link.href.includes(`${activePage}`)){
+      link.classList.add("actives")
+   }
+})
